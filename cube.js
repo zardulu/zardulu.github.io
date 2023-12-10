c517827e332412e3a5364cef6615b90d72a4820d
Anshul <61840818+zardulu@users.noreply.github.com>
2023-12-10 23:29:39 +0530

    Add animation

---
+++ cube.js
@@ -0,0 +1,31 @@
+import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, 1, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( 300,300);
+document.body.appendChild( renderer.domElement );
+document.getElementById('canvas-container').appendChild(renderer.domElement);
+
+const geometry = new THREE.OctahedronGeometry( 1, 0 );
+const material = new THREE.LineBasicMaterial( {
+	color: 0xffffff,
+	
+} );
+const cube = new THREE.LineSegments( new THREE.EdgesGeometry(geometry),
+ material );
+
+scene.add( cube );
+
+camera.position.z = 2;
+camera.position.y = 0.3;
+
+function animate() {
+
+    requestAnimationFrame( animate );   
+    cube.rotation.y += 0.010; 
+    renderer.render( scene, camera );
+
+}
+
+animate();
